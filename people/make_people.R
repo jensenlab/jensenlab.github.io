76cf9e6c9c004ec683f5c688b6ac16e41d0761e0
Paul Jensen <software@jensenlab.net>
2018-03-07 22:39:11 -0600

    type

--- people/make_people.R
+++ people/make_people.R
@@ -21,7 +21,7 @@ people <- tibble::tribble(
   "Garrett Chou", "Undergraduate Student", "Garrett_Chou.png",
   "William Herbert", "Undergraduate Student", "Will_Herbert.png",
   "Matthew Tang", "Undergraduate Student", "Matthew_Tang.png",
-  "Adam Dama", "Undergraduate Student", "Adam_Dama.png",
+  "Adam Dama", "Undergraduate Student", "Adam_Dama.jpg",
   "Akash Mukherhee", "Undergraduate Student", "Akash_Mukherjee.png"
 )
 
